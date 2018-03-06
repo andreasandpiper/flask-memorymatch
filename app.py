ad4335afe3e30e7dde7082449709605a107b72e8
Andrea <andrea.wayte@gmail.com>
2018-03-06 11:55:20 -0800

    add logging

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, abort, redirect, url_for
+import logging
 
 
 app = Flask(__name__)
 
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 @app.route('/')
 def index():
