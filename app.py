3b573a1b21ad1f24c9018ed4cbd90b5f8751760a
Andrea <andrea.wayte@gmail.com>
2018-03-06 12:24:57 -0800

    add routing

--- app.py
+++ app.py
@@ -8,12 +8,52 @@ app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
+
+current_game = game.Game(0)
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+@app.route('/game', methods=["POST", 'GET'])
+def game():
+    print current_game
+    return render_template('game.html', attempts = current_game.attempts, wins = current_game.games_won)
+
+@app.route('/update_score', methods=["POST"])
+def update_score():
+    score = current_game.add_match()
+    data = {}
+    data['matches'] = score
+    data['total'] = 9
+    return jsonify(data)
+
+@app.route('/attempt', methods=["POST"])
+def attempt():
+    attempts = current_game.attempt()
+    data = {}
+    data['attempts'] = attempts
+    data['matches'] = current_game.matches
+    return jsonify(data)
+
+@app.route('/winner', methods=["POST"])
+def winner():
+    data = {}
+    if current_game.matches == 9:
+        data['status'] = "true"
+        current_game.new_game()
+        current_game.win()
+    else:
+        data['status'] = "false"
+        current_game.matches = 0
+    return jsonify(data)
 
 
 
 
 if __name__ == '__main__': app.run(debug=True)
+
